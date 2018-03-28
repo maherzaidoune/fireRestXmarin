e34c0051ec229fe370f165ed1cdcd079c2860f98
zaidoune <maherzaidoune@gmail.com>
2018-03-28 16:10:52 +0100

    Add project files.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+        
+        private string url = "https://essths-9bd84.firebaseio.com/users.json";
+        private HttpClient client = new HttpClient();
+
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+        public async void addData(Object sender, EventArgs e)
+        {
+            var newUser = new user { name = Username.Text, lastName = Lastname.Text };
+            var content = JsonConvert.SerializeObject(newUser);
+            await client.PostAsync(url,new  StringContent(content));
+
+        }
+    
+        public async  void getData(Object sender, EventArgs e)
+        {
+            var firedata = await client.GetStringAsync(url);
+            data.Text = firedata.ToString();
+        }
+    }
+}
---
+++ App1/App1/user.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public class user
+    {
+        public string name { get; set; }
+        public string lastName { get; set; }
+    }
+}
